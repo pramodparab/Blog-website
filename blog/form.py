c00546ffe0af1a79b7add3ce97e2f4d5f181c1ca
Pramod <pparab0@gmail.com>
2019-12-16 23:32:10 +0530

    add comment form

--- blog/form.py
+++ blog/form.py
@@ -1,9 +1,17 @@
 from django import forms
+from .models import Comment
 
-
+#email send form
 class EmailPostForm(forms.Form):
         name = forms.CharField(max_length  = 25)
         email = forms.EmailField()
         to = forms.EmailField()
         comment = forms.CharField(required = False ,
                                   widget = forms.Textarea)
+
+#comment form
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email','body')
